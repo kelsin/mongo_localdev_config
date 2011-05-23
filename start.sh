f33d9b08a8a0c4dfbcf92a88c2757badd5d0b715
Christopher Giroir <kelsin@valefor.com>
2011-05-23 11:57:33 -0400

    Added check for env var and correct pid files

--- start.sh
+++ start.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+if [ -z "$SWMONGO" ]; then
+    echo "Need to set SWMONGO to point to your mongo directory"
+    exit 1
+fi
+
 echo "* Starting mongo servers"
 mongod --replSet shard1 --port 27021 --shardsvr --master --dbpath $SWMONGO/shard1/master --pidfilepath $SWMONGO/pids/shard1-master.pid --logpath $SWMONGO/logs/shard1-master.log --logappend &
-mongod --replSet shard1 --port 27022 --shardsvr --slave  --dbpath $SWMONGO/shard1/slave1 --pidfilepath $SWMONGO/pids/shard1-slash1.pid --logpath $SWMONGO/logs/shard1-slave1.log --logappend --source localhost:27021 &
-mongod --replSet shard1 --port 27023 --shardsvr --slave  --dbpath $SWMONGO/shard1/slave2 --pidfilepath $SWMONGO/pids/shard1-slash2.pid --logpath $SWMONGO/logs/shard1-slave2.log --logappend --source localhost:27021 &
+mongod --replSet shard1 --port 27022 --shardsvr --slave  --dbpath $SWMONGO/shard1/slave1 --pidfilepath $SWMONGO/pids/shard1-slave1.pid --logpath $SWMONGO/logs/shard1-slave1.log --logappend --source localhost:27021 &
+mongod --replSet shard1 --port 27023 --shardsvr --slave  --dbpath $SWMONGO/shard1/slave2 --pidfilepath $SWMONGO/pids/shard1-slave2.pid --logpath $SWMONGO/logs/shard1-slave2.log --logappend --source localhost:27021 &
 mongod --replSet shard2 --port 27024 --shardsvr --master --dbpath $SWMONGO/shard2/master --pidfilepath $SWMONGO/pids/shard2-master.pid --logpath $SWMONGO/logs/shard2-master.log --logappend &
 mongod --replSet shard2 --port 27025 --shardsvr --slave  --dbpath $SWMONGO/shard2/slave1 --pidfilepath $SWMONGO/pids/shard2-slave1.pid --logpath $SWMONGO/logs/shard2-slave1.log --logappend --source localhost:27024 &
 mongod --replSet shard2 --port 27026 --shardsvr --slave  --dbpath $SWMONGO/shard2/slave2 --pidfilepath $SWMONGO/pids/shard2-slave2.pid --logpath $SWMONGO/logs/shard2-slave2.log --logappend --source localhost:27024 &
@@ -15,5 +20,5 @@ echo "- Waiting for config server to start ..."
 sleep 5
 
 echo "* Starting router"
-mongos --port 27017 --configdb localhost:27019 --chunkSize 1 --pidfilepath $SWMONGO/pids/router.pig --logpath $SWMONGO/logs/router.log --logappend &
+mongos --port 27017 --configdb localhost:27019 --chunkSize 1 --pidfilepath $SWMONGO/pids/router.pid --logpath $SWMONGO/logs/router.log --logappend &
 
--- stop.sh
+++ stop.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-kill -15 $SWMONGO/pids/shard1-master.pid
-kill -15 $SWMONGO/pids/shard1-slave1.pid
-kill -15 $SWMONGO/pids/shard1-slave2.pid
-kill -15 $SWMONGO/pids/shard2-master.pid
-kill -15 $SWMONGO/pids/shard2-slave1.pid
-kill -15 $SWMONGO/pids/shard2-slave2.pid
-kill -15 $SWMONGO/pids/config.pid
-kill -15 $SWMONGO/pids/router.pid
+if [ -z "$SWMONGO" ]; then
+    echo "Need to set SWMONGO to point to your mongo directory"
+    exit 1
+fi
+
+kill -15 `cat $SWMONGO/pids/shard1-master.pid`
+kill -15 `cat $SWMONGO/pids/shard1-slave1.pid`
+kill -15 `cat $SWMONGO/pids/shard1-slave2.pid`
+kill -15 `cat $SWMONGO/pids/shard2-master.pid`
+kill -15 `cat $SWMONGO/pids/shard2-slave1.pid`
+kill -15 `cat $SWMONGO/pids/shard2-slave2.pid`
+kill -15 `cat $SWMONGO/pids/config.pid`
+kill -15 `cat $SWMONGO/pids/router.pid`
