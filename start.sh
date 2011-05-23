c22ca0538d0deac9eb0995684e97d9afcb5973a7
Christopher Giroir <kelsin@valefor.com>
2011-05-23 17:09:25 -0400

    Added -vvv and --rest for admin server

--- start.sh
+++ start.sh
@@ -6,15 +6,15 @@ if [ -z "$MONGO" ]; then
 fi
 
 echo "* Starting mongo servers"
-mongod --replSet shard1 --port 27021 --shardsvr --master --dbpath $MONGO/shard1/master --pidfilepath $MONGO/pids/shard1-master.pid --logpath $MONGO/logs/shard1-master.log --logappend &
-mongod --replSet shard1 --port 27022 --shardsvr --slave  --dbpath $MONGO/shard1/slave1 --pidfilepath $MONGO/pids/shard1-slave1.pid --logpath $MONGO/logs/shard1-slave1.log --logappend --source localhost:27021 &
-mongod --replSet shard1 --port 27023 --shardsvr --slave  --dbpath $MONGO/shard1/slave2 --pidfilepath $MONGO/pids/shard1-slave2.pid --logpath $MONGO/logs/shard1-slave2.log --logappend --source localhost:27021 &
-mongod --replSet shard2 --port 27024 --shardsvr --master --dbpath $MONGO/shard2/master --pidfilepath $MONGO/pids/shard2-master.pid --logpath $MONGO/logs/shard2-master.log --logappend &
-mongod --replSet shard2 --port 27025 --shardsvr --slave  --dbpath $MONGO/shard2/slave1 --pidfilepath $MONGO/pids/shard2-slave1.pid --logpath $MONGO/logs/shard2-slave1.log --logappend --source localhost:27024 &
-mongod --replSet shard2 --port 27026 --shardsvr --slave  --dbpath $MONGO/shard2/slave2 --pidfilepath $MONGO/pids/shard2-slave2.pid --logpath $MONGO/logs/shard2-slave2.log --logappend --source localhost:27024 &
+mongod -vvv --rest --replSet shard1 --port 27021 --shardsvr --master --dbpath $MONGO/shard1/master --pidfilepath $MONGO/pids/shard1-master.pid --logpath $MONGO/logs/shard1-master.log --logappend &
+mongod -vvv --rest --replSet shard1 --port 27022 --shardsvr --slave  --dbpath $MONGO/shard1/slave1 --pidfilepath $MONGO/pids/shard1-slave1.pid --logpath $MONGO/logs/shard1-slave1.log --logappend --source localhost:27021 &
+mongod -vvv --rest --replSet shard1 --port 27023 --shardsvr --slave  --dbpath $MONGO/shard1/slave2 --pidfilepath $MONGO/pids/shard1-slave2.pid --logpath $MONGO/logs/shard1-slave2.log --logappend --source localhost:27021 &
+mongod -vvv --rest --replSet shard2 --port 27024 --shardsvr --master --dbpath $MONGO/shard2/master --pidfilepath $MONGO/pids/shard2-master.pid --logpath $MONGO/logs/shard2-master.log --logappend &
+mongod -vvv --rest --replSet shard2 --port 27025 --shardsvr --slave  --dbpath $MONGO/shard2/slave1 --pidfilepath $MONGO/pids/shard2-slave1.pid --logpath $MONGO/logs/shard2-slave1.log --logappend --source localhost:27024 &
+mongod -vvv --rest --replSet shard2 --port 27026 --shardsvr --slave  --dbpath $MONGO/shard2/slave2 --pidfilepath $MONGO/pids/shard2-slave2.pid --logpath $MONGO/logs/shard2-slave2.log --logappend --source localhost:27024 &
 
 echo "* Starting config server"
-mongod --configsvr --port 27019 --dbpath $MONGO/config --pidfilepath $MONGO/pids/config.pid --logpath $MONGO/logs/config.log --logappend &
+mongod -vvv --rest --configsvr --port 27019 --dbpath $MONGO/config --pidfilepath $MONGO/pids/config.pid --logpath $MONGO/logs/config.log --logappend &
 
 echo "- Waiting for config server to start ..."
 sleep 5
